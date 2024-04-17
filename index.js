eeca2091be7c6ac9b87e6fa8ddff256bfb023930
Eliza Cohen <elizacohen5@gmail.com>
2024-04-17 09:21:09 -0400

    complete intro to js 2 objects lab

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution in this file!
+const employee = {
+    name: "Pistachio",
+    streetAddress: "820 West End Ave"
+}
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    const updatedEmployee = {...obj};
+    updatedEmployee[key] = value;
+    return updatedEmployee;
+}
+
+// console.log(updateEmployeeWithKeyAndValue(employee, "name", "Eliza"));
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+// console.log(destructivelyUpdateEmployeeWithKeyAndValue(employee, "name", "Eliza"));
+
+function deleteFromEmployeeByKey(employee, key) {
+    const updatedEmployee = {...employee};
+    delete updatedEmployee[key];
+    return updatedEmployee;
+}
+
+// console.log(deleteFromEmployeeByKey(employee, "streetAddress"));
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
